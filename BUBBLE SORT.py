23d944c24371b02e6b5f47c78e8f9769950b9932
arunimacho09 <162032351+arunimacho09@users.noreply.github.com>
2024-03-04 20:13:36 +0530

    Update BUBBLE SORT.py

--- BUBBLE SORT.py
+++ BUBBLE SORT.py
@@ -5,8 +5,11 @@
 
 
 def bubble_sort(b_list):
+    #Looping through the entire length of list
     for i in range(len(b_list)):
+       #loop for comparing the adjacent elements of the list 
         for j in range(0,len(b_list)-i-1):
+            # Condition given so that the elements are swapped in ascending order
             if (b_list[j] > b_list[j+1]):
                 b_list[j],b_list[j+1] = b_list[j+1],b_list[j]
     return b_list
